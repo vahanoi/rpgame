991cc8ef08e1f4af63ce5c7ee3b7583d53f748bb
vahanoi <vahanoi@gmail.com>
2018-10-30 19:28:50 +0000

    Copy source from Game01 RPGame will be a default project

---
+++ rpgame/player/player.py
@@ -0,0 +1,23 @@
+'''
+Created on 27 Oct 2018
+
+@author: vahanoi
+'''
+
+class Player:
+    '''
+    classdocs
+    '''
+# TODO: create object Player with all details
+
+    def __init__(self, name, emailaddress):
+        '''
+        Constructor
+        '''
+        
+ 
+    #TODO: test TODO
+        age=0
+        return(None)
+    
+    
