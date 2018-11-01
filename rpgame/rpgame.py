9ce2d4132b846e33217a7bb23e8bdc3df0cd2123
vahanoi <vahanoi@gmail.com>
2018-11-01 19:55:11 +0000

    updates plus SQL query

--- rpgame/rpgame.py
+++ rpgame/rpgame.py
@@ -30,24 +30,30 @@ def main():
     '''
     db = sqlite3.connect('game.db')
     c = db.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS player
+    c.execute('''CREATE TABLE IF NOT EXISTS character
                     (id,name,race,experience)''')
 
 # main loop
     while True:
-        splashscreen() #general info about a game - text
+        splashscreen() # General info about a game - text
         key = input('Do you need me to start the Game [Y/N]')
-        if key == 'N' or key == 'n':
-            db.close()
+        if key == 'N' or key == 'n': # N to exit
+            db.close() # Closing database
             print('\n\nClosing awesome world!!!!!\n\n')
             sys.exit(0)
         elif key=='Y' or key=='y':
-            suname=input(f"What will be your name? ")
-            print(f'Chosen name: {suname}')
+            print('Loading the RPGame......\n\n')
+            chname=input(f"What will be your name> ")
+            print(f'Chosen name: {chname} Checking if exists.')
+            # TODO: check if name exists in DB if yes load it if no create new
+            sql = list() # create SQL query as a list
+            sql.append('SELECT * FROM character WHERE name=\"%s\"' % chname) # join string elements and variables together
+            print ("".join(sql))
+            c.execute ('')
             sracename=input(f"chose a race[Human/Drakl/Blump] ")
-            print(f'Chosen name: {sracename}')
-        else: print('have you read info yet???')
-    ch = Character(suname,sracename,"")
+            print(f'Chosen name> {sracename}')
+        else: print('\nHave you read info yet???')
+    ch = Character(chname,sracename,"")
     
 if __name__ == '__main__':
     main()
