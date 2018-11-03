940897de0ce55bd1d4d3e228a4f70d37710b9c1b
vahanoi <vahanoi@gmail.com>
2018-11-03 00:48:08 +0000

    db access code and few comments what need to be done

--- rpgame/rpgame.py
+++ rpgame/rpgame.py
@@ -26,6 +26,7 @@ Please chose your Name and Race below \n\n
 def main():
     '''
     Create sqlite database
+    TODO: make DB separate module??
     TODO: DB as a object so starting and initial checks in a separate module
     TODO: indexing
     CREATE UNIQUE INDEX `` ON `character` (
@@ -35,11 +36,22 @@ def main():
     c = db.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS character
                     (id,name,race,experience)''')
-
+    '''
+    TODO: Create Race table with race parameters (max 3)
+    id, 
+    rname,
+    race modifiers - strength, luck, dexterity, Stamina, Social, wisdom
+    
+    '''
+    debug=True
 # main loop
     while True:
-        splashscreen() # General info about a game - text
+        # General info about a game - text
+        splashscreen() 
         key = input('Do you need me to start the Game [Y/N]')
+        '''
+        Exit game procedure - db closing
+        '''
         if key == 'N' or key == 'n': # N to exit
             db.close() # Closing database
             print('\n\nClosing awesome world!!!!!\n\n')
@@ -47,15 +59,21 @@ def main():
         elif key=='Y' or key=='y':
             print('Loading the RPGame......\n\n')
             chname=input(f"What will be your name> ")
-            print(f'Chosen name: {chname} Checking if exists.')
+            t=(chname,)
+            if debug==True: 
+                print(f'Chosen name: {chname} Checking if exists.')
             # TODO: check if name exists in DB if yes load it if no create new
-            sql = list() # create SQL query as a list
-            sql.append('SELECT * FROM character WHERE name=\"%s\"' % chname) # join string elements and variables together
-            print ("".join(sql))
-            c.execute ('')
-            sracename=input(f"chose a race[Human/Drakl/Blump] ")
-            print(f'Chosen name> {sracename}')
-        else: print('\nHave you read info yet???')
+            c.execute ('SELECT * FROM character WHERE name=?',t)
+            result=c.fetchone()
+            
+            if debug==True:
+                print(result)
+            if result==None:
+                sracename=input(f"chose a race[Human/Drakl/Blump] ")
+                print(f'Chosen name> {sracename}')
+            else:
+                input('Character exists. Load Character?')
+        else: print('\n\nHave you read info yet???')
     ch = Character(chname,sracename,"")
     
 if __name__ == '__main__':
